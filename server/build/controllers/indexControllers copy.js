40a2dcdbb894c5ebace43f0e8f14ca00d35f16b5
mckimssongg <soymckimsson286@gmail.com>
2023-03-28 03:10:33 -0600

    delete build

--- server/build/controllers/indexControllers copy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexController = void 0;
-class indexControllers {
-    index(req, res) {
-        res.send('Hello World!');
-    }
-}
-exports.indexController = new indexControllers();
--- server/build/controllers/indexControllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexController = void 0;
-class indexControllers {
-    index(req, res) {
-        res.json({ text: 'API is /api/' });
-    }
-}
-exports.indexController = new indexControllers();
